5a0dacbb6abe3cd32b02d1d3bf0cc6e3ef4a9240
Magnus Sandén <thefreid@gmail.com>
2020-07-10 23:05:15 +0200

    Add basic ggez scaffolding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use ggez::{self, event::EventHandler, Context, GameResult};
+
+const SCREEN_SIZE: (f32, f32) = (800.0, 600.0);
+
+struct MainState {}
+
+impl EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        Ok(())
+    }
+
+    fn draw(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        Ok(())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (mut ctx, mut event_loop) = ggez::ContextBuilder::new("NoCtrl", "Weirdo")
+        .window_setup(
+            ggez::conf::WindowSetup::default()
+                .title("NoCtrl")
+                .vsync(true),
+        )
+        .window_mode(ggez::conf::WindowMode::default().dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1))
+        .build()
+        .unwrap();
+
+    let mut main_state = MainState{};
+
+    match ggez::event::run(&mut ctx, &mut event_loop, &mut main_state) {
+        Ok(_) => println!("Exited cleanly."),
+        Err(e) => println!("Error occured: {}", e),
+    }
 }
