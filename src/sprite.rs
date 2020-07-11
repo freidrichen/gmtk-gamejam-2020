4a23e222fbd031dae1cfd43ba5a7e6c2154cbf1e
Jonathan Karlsson <karlsjona@gmail.com>
2020-07-11 20:03:16 +0200

    Update sprites

--- src/sprite.rs
+++ src/sprite.rs
@@ -23,16 +23,16 @@ pub fn screen_pos(tile_pos: Point2<usize>) -> Point2<f32> {
 
 pub fn get_sprite(sprite_type: SpriteType) -> Rect {
     let index = match sprite_type {
-        SpriteType::Player => 2,
-        SpriteType::Wall => 1,
-        SpriteType::Floor => 0,
+        SpriteType::Player => 8,
+        SpriteType::Wall => 0,
+        SpriteType::Floor => 1,
     };
     let row = index / NUM_SPRITES_X;
     let col = index % NUM_SPRITES_X;
     assert!(row < NUM_SPRITES_Y);
     Rect::new(
-        row as f32 / NUM_SPRITES_X as f32,
-        col as f32 / NUM_SPRITES_Y as f32,
+        col as f32 / NUM_SPRITES_X as f32,
+        row as f32 / NUM_SPRITES_Y as f32,
         1.0 / NUM_SPRITES_X as f32,
         1.0 / NUM_SPRITES_Y as f32,
     )
